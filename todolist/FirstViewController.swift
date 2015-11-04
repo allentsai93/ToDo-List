03d09b538dbd38ac283d3b2e20899761f2557902
Allen Tsai <allentsai93@gmail.com>
2015-11-04 08:48:28 -0800

    add implemented

--- todolist/FirstViewController.swift
+++ todolist/FirstViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var toDoList = [String]()
 
+class FirstViewController: UIViewController, UITableViewDelegate {
+
+    @IBOutlet var toDoListTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,24 @@ class FirstViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return toDoList.count
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = toDoList[indexPath.row]
+        return cell
+    }
+    override func viewDidAppear(animated: Bool) {
+        toDoListTable.reloadData()
+    }
 
 }
 
